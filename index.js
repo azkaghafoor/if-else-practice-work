23f37f81d9bb2887f81582af9f5b3efb0f8a1909
azkaghafoor <azkaghafoor17@gmail.com>
2025-05-27 16:36:50 -0800

    practice work done

---
+++ index.js
@@ -0,0 +1,105 @@
+// Example 1
+let age = 18;
+if (age > 16) {
+  console.log("You can cross the road");
+} else {
+  console.log("You can not cross the road");
+}
+// Example 2
+let time = 9;
+if (time < 12) {
+  console.log("Good Morning");
+} else {
+  console.log("Good Afternoon");
+}
+// Example 3
+let student = false;
+if (student) {
+  console.log("You are Student");
+} else {
+  console.log("You are not Student");
+}
+// Example 4
+let officeTime = 8;
+if (officeTime > 8) {
+  console.log("You can tired from Work");
+} else {
+  console.log("You can not tired from Work");
+}
+// Example 5
+let lunchTime = 1;
+if (lunchTime < 12) {
+  console.log("Its not lunch time yet");
+} else {
+  console.log("its lunch time yet");
+}
+// Example 6
+let women = true;
+if (women) {
+  console.log("You can sit in Bus");
+} else {
+  console.log("You can stand in Bus");
+}
+// Example 7
+let grade = "B";
+if (grade === "A") {
+  console.log("You are Good in Studies");
+} else {
+  console.log("You are not Good in Studies");
+}
+// Example 8
+let number = 50;
+if (number < 55) {
+  console.log("You passed the examination");
+} else {
+  console.log("You failed the examination");
+}
+// Example 9
+let rating = 4;
+if (rating > 3) {
+  console.log("Good rating");
+} else {
+  console.log("Bad rating");
+}
+// Example 10
+let batteryLevel = 80;
+if (batteryLevel > 60) {
+  console.log("Your battery level is good");
+} else {
+  console.log("Your battery level is not good");
+}
+// Example 11
+let internetConnection = "connected";
+if (internetConnection === "connected") {
+  console.log("You are online");
+} else {
+  console.log("You are offline");
+}
+// Example 12
+let weather = "rainy";
+if (weather === "rainy") {
+  console.log("I like the weather");
+} else {
+  console.log("I don't like the weather");
+}
+// Example 13
+let score = 90;
+if (score > 70) {
+  console.log("Grade: A");
+} else {
+  console.log("Grade: B");
+}
+// Example 14
+let temperature = 40;
+if (temperature > 30) {
+  console.log("Its a hot day");
+} else {
+  console.log("Its a cold day");
+}
+// Example 15
+let fruit = "bnana";
+if (fruit === "mango") {
+  console.log("I like fruit");
+} else {
+  console.log("i don't like fruit");
+}
